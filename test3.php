38febe94bd336601d016357b994dfae8e3168ddf
胡一天 <30338315+nebulahu@users.noreply.github.com>
2019-12-05 16:08:52 +0800

    Delete test3.php

--- test3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$http = new Swoole\Http\Server('0.0.0.0',9501);
-$http->on('Request',function($request,$response){
-  var_dump($request->get);
-  $response->header("Content-type","text/html;charset=utf-8");
-  $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-});
-$http->start();
-?>
